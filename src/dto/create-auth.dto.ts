8ea978a342ea9a677f505352886855478029ae2d
Adi <adindo48@gmail.com>
2023-11-11 10:40:16 +0700

    validation on DTO

--- src/dto/create-auth.dto.ts
+++ src/dto/create-auth.dto.ts
@@ -1,12 +1,24 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
+
 export class AuthLoginDto {
   id: number;
+
+  @IsNotEmpty()
   username: string;
+
+  @IsNotEmpty()
   password: string;
 }
 
 export class AuthRegisterDto {
   id: number;
+
+  @IsNotEmpty()
   username: string;
+
+  @IsNotEmpty()
   password: string;
+
+  @IsEmail()
   email: string;
 }
